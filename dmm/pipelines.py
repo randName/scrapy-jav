cb2488ce17dd31b63eeef60fda1d0f3814eb1c39
Shun Git <kwokshungit@gmail.com>
2016-12-30 14:33:40 +0000

    clean up spiders

--- dmm/pipelines.py
+++ dmm/pipelines.py
@@ -5,62 +5,86 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from os import fsync
+from xmlrpc.client import ServerProxy
+
+TOKEN = 'token:asdfghjkl'
+PKG_URL = 'http://pics.dmm.co.jp/digital/video/{0}/{0}pl.jpg'
+SMP_URL = 'http://pics.dmm.co.jp/digital/video/{0}/{0}jp-{1}.jpg'
+KEYS = ('day', 'cid', 'pkg', 'runtime', 'maker', 'label', 'series', 'director')
 
 class DmmPipeline(object):
 
     def open_spider(self, spider):
-        self.articles = {}
-        self.videos = {}
-        self.misc = open('misc.csv', 'w')
-        self.keyword = open('keywords.csv', 'w')
-        self.actress = open('actresses.csv', 'w')
+        self.misc = open('data/titles.csv', 'w')
+        self.related = open('data/related.csv', 'w')
+        for c in ('joins', 'videos', 'articles'):
+            setattr(self, c, {})
+        self.videocount = 0
+        self.c = ServerProxy('http://localhost:6800/rpc', allow_none=True)
 
     def close_spider(self, spider):
-        for f in self.articles.values():
-            f.close()
-        for f in self.videos.values():
-            f.close()
         self.misc.close()
-        self.keyword.close()
-        self.actress.close()
+        self.related.close()
+        for c in ('joins', 'videos', 'articles'):
+            for f in getattr(self, c).values():
+                f.close()
 
-    def process_item(self, item, spider):
-        if 'article' in item:
-            return item
-            article = item['article']
-            f = self.get_file('articles', article)
-            print('{id},{name}'.format(**item), file=f)
-        else:
-            if '/' in item['date']:
-                d = item['date'].split('/')
-            else:
-                d = ('no', 'date', 0)
-            ks = ('cid', 'runtime', 'maker', 'label', 'series', 'director')
-            s = d[2] + ',' + ','.join(self.get_keys(item, ks))
-            print(s, file=self.get_file('videos', '_'.join(d[:2])))
-            ks = ('cid', 'pkg', 'samples', 'title')
-            s = ','.join(self.get_keys(item, ks))
-            print(s, file=self.misc)
-            for k in ('keyword', 'actress'): self.m2m(k, item)
-
-    def get_keys(self, item, keys):
-        for k in keys:
-            try:
-                yield str(item[k])
-            except KeyError:
-                yield ''
-
-    def m2m(self, k, item):
-        try:
-            s = '\n'.join('%s,%s' % (item['cid'], i) for i in item[k])
-            print(s, file=getattr(self, k))
-        except KeyError:
-            pass
+    def adl(self, url):
+        self.c.aria2.addUri(TOKEN, (url,))
 
     def get_file(self, t, k):
         try:
             return getattr(self, t)[k]
         except KeyError:
-            f = open('%s/%s.csv' % (t, k), 'w')
+            f = open('data/%s/%s.csv' % (t, k), 'w')
             getattr(self, t)[k] = f
         return f
+
+    def process_item(self, item, spider):
+        if 'article' in item:
+            article = item['article']
+            f = self.get_file('articles', article)
+            print('{id},{name}'.format(**item), file=f)
+        elif 'related' in item:
+            print(','.join(item['related']), file=self.related)
+        else:
+            self.adl(PKG_URL.format(item['pkg']))
+            for i in range(item['samples']):
+                self.adl(SMP_URL.format(item['pkg'], i+1))
+
+            if item['date']:
+                fn = '_'.join(item['date'][:2])
+                item['day'] = item['date'][2]
+            else:
+                fn = 'no_date'
+
+            print(
+                ','.join(str(item.get(k, '')) for k in KEYS),
+                file=self.get_file('videos', fn)
+            )
+
+            print(
+                ','.join(str(item.get(k, '')) for k in ('cid', 'title', 'description')),
+                file=self.misc
+            )
+
+            for k in ('keyword', 'actress', 'histrion'):
+                if k not in item: continue
+                print(
+                    '\n'.join('%s,%s' % (i, item['cid']) for i in item[k]),
+                    file=self.get_file('joins', k)
+                )
+
+            self.videocount += 1
+
+            if ( self.videocount % 1000 == 0 ):
+                print('Scraped %d videos, writing data to disk' % self.videocount)
+                for c in ('joins', 'videos', 'articles'):
+                    for f in getattr(self, c).values():
+                        f.flush()
+                        fsync(f.fileno())
+                self.related.flush()
+                fsync(self.related.fileno())
+                self.misc.flush()
+                fsync(self.misc.fileno())
--- dmm/spiders/video_spider.py
+++ dmm/spiders/video_spider.py
@@ -1,17 +1,18 @@
-import scrapy
 import re
 
-from urllib.parse import urlsplit, urljoin
+from scrapy import Spider, Request
 from os.path import dirname, basename
+from urllib.parse import urlsplit
+
 from . import *
 
+RTM_RE = re.compile(r'(\d+)分')
+ART_RE = re.compile(r'article=(?P<article>\w+)/id=(?P<id>\d+)')
+CID_RE = re.compile(r'/(?P<service>\w+)/(?P<shop>\w+)/-/detail/=/cid=(?P<cid>\w+)')
 
-class VideoSpider(scrapy.Spider):
+
+class VideoSpider(Spider):
     name = 'videos'
-
-    rt = re.compile(r'(\d+)分')
-    rowtag = { '売日': 'date', '品番': 'cid' }
-    art_id = re.compile(r'article=(\w+)/id=(\d+)')
 
     def __init__(s, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -27,80 +28,106 @@ class VideoSpider(scrapy.Spider):
             p = {}
 
         p['sort'] = kwargs['sort'] if 'sort' in kwargs else 'release_date'
-        s.start_urls = realm_urls('-/list/=/%s' % urlparams(**p), realm=r)
 
-    def article_links(s, links):
-        articles = { 'links': [], 'id': [] }
-        i = {}
-        for l in links:
-            try:
-                a = s.art_id.search(l).groups()
-                if 'article' not in i:
-                    i['article'] = a[0]
-                i['id'] = int(a[1])
-                articles['links'].append((l, i))
-                articles['id'].append(i['id'])
-            except (TypeError, AttributeError):
-                pass
-        try:
-            articles['article'] = i['article']
-        except KeyError:
-            pass
-        return articles
+        s.start_urls = realm_urls('-/list/=/%s' % urlparams(**p), realm=(0,))
+
+    def maker_urls(s, p):
+        p['article'] = 'maker'
+        with open ('makers_by_videocount','r') as f:
+            makers = tuple( m.strip('\n') for m in f )
+
+        for m in makers:
+            p['id'] = m
+            yield realm_urls('-/list/=/%s' % urlparams(**p), realm=(0,))[0]
 
     def parse(s, r):
         yield from pagelist(r.css('div.list-boxpagenation'), callback=s.parse)
         yield from pagelist(r, selector='p.tmb', callback=s.parse_video)
 
+    def parse_mutual(s, r):
+        related = tuple(s.safegroupdict(r.css(href).extract(), CID_RE))
+        yield { 'related': related+(r.meta['item'],) }
+
+    def parse_article(s, r):
+        item = r.meta['item']
+        span = r.css('p.headwithelem span::text').extract()
+        item['name'] = ''.join(span[:2]).split('\xa0')[-1].strip('\n')
+        yield item
+
     def parse_video(s, r):
         vid = {
-            'url': r.url,
             'title': r.css('h1::text').extract_first(),
-            'rating': r.css("div.d-review__points strong::text").extract(),
+            'description': s.get_description(r.css('div.mg-b20.lh4')),
+            #'rating': r.css("div.d-review__points strong::text").extract(),
+            'samples': len(r.css('a[id*=sample-image] %s' % isrc).extract()),
+            'pkg': urlsplit(r.css('img.tdmm::attr(src)').extract_first()).path,
         }
 
-        pkg = urlsplit(r.css('img.tdmm::attr(src)').extract_first()).path
-        vid['pkg'] = basename(dirname(pkg))
+        table = { k: v for k, v in s.get_table(r) }
 
-        samples = r.css('a[id*=sample-image] %s' % isrc).extract()
-        if samples:
-            vid['samples'] = len(samples)
-            vid['samples_path'] = dirname(urlsplit(samples[0]).path)
+        for rq in table['requests']:
+            yield rq
+        del table['requests']
 
-        d = r.css("div.mg-b20.lh4")
-        desc = d.css("p.mg-b20::text").extract_first()
+        url = CID_RE.search(r.url).groupdict()
+        u = r.urljoin('/misc/-/mutual-link/ajax-index/=/'+urlparams(**url))
+        yield Request(u, callback=s.parse_mutual, meta={'item': url})
+
+        #vid['samples_path'] = dirname(urlsplit(samples[0]).path)
+        yield {**vid, **table}
+
+    def safegroupdict(self, strings, pattern):
+        for s in strings:
+            try:
+                yield pattern.search(s).groupdict()
+            except AttributeError:
+                print(s)
+                pass
+
+    def get_description(self, d):
+        desc = d.css('p.mg-b20::text').extract_first()
         if not desc:
-            desc = d.css("::text").extract_first()
-        vid['description'] = desc.strip('\n')
+            desc = d.css('::text').extract_first()
+        return desc.strip('\n')
+
+    def get_articles(self, links):
+        for l in links:
+            try:
+                a = ART_RE.search(l).groupdict()
+                a['url'] = l
+                yield a
+            except (KeyError, TypeError, AttributeError):
+                pass
+
+    def get_table(s, r):
+        m2m = ('actress', 'keyword', 'histrion')
+        requests = []
 
         for row in r.css('div.page-detail table table tr'):
-            a = s.article_links(row.css(href).extract())
-            for i in a['links']:
-                u = urljoin(r.url, i[0])
-                rq = scrapy.Request(u, callback=s.parse_article)
-                rq.meta['item'] = i[1]
-                yield rq
+            articles = tuple(s.get_articles(row.css(href).extract()))
 
-            if a['id']:
-                k = a['article']
-                vid[k] = a['id'] if k in ('actress', 'keyword') else a['id'][0]
+            for a in articles:
+                requests.append(Request(r.urljoin(a['url']),
+                    callback=s.parse_article, meta={'item': a}))
+
+            if articles:
+                a = articles[0]
+                a_id = int(a['id'])
+                if a['article'] in m2m:
+                    a_id = tuple(int(a['id']) for a in articles)
+                yield a['article'], a_id
                 continue
 
             d = row.css('td::text').extract()
             try:
-                vid[s.rowtag[d[0][-3:-1]]] = d[1].strip('\n')
-                continue
-            except (IndexError, KeyError):
-                pass
-            try:
-                vid['runtime'] = int(s.rt.match(d[1]).group(1))
+                yield 'runtime', int(RTM_RE.match(d[1]).group(1))
             except (IndexError, AttributeError):
                 pass
+            try:
+                if d[0][-3:-1] == '売日':
+                    date_str = d[1].strip('\n') if '/' in d[1] else ''
+                    yield 'date', date_str.split('/')
+            except (IndexError, KeyError):
+                pass
 
-        yield vid
-
-    def parse_article(s, r):
-        item = r.meta['item']
-        span = r.css("p.headwithelem span::text").extract()
-        item['name'] = ''.join(span[:2]).split('\xa0')[-1].strip('\n')
-        yield item
+        yield 'requests', requests
